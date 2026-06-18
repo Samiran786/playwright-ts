ade2d07346e7d58dbfcc8ff50039175e714d778e
Samiran786 <bsamibane786@gmail.com>
2026-06-18 15:55:03 +0530

    calender selector test and utils creation

--- fixtures/bookMyShow/ticketBooking.spec.ts
+++ fixtures/bookMyShow/ticketBooking.spec.ts
@@ -1,12 +1,18 @@
 import {test,expect} from "@playwright/test";
+import { calenderSelector} from "../../../utils/calenderSelector.utils";
 
 test('Booking tickets', async function({page}){
-    const datePicker = page.locator(`//td/div[contains(text(),'18')]`);
+    const rootCal = page.locator(`.datepicker:not(.hidden)`);
+    const datePicker = rootCal.locator(`div.day:not(.disabled)`);
+    const monthChecker = rootCal.locator(`.datepicker-months span`);
+    const deptOption = page.getByLabel('Departure Date').nth(0);
+    const returnOption = page.getByLabel('Return Date').nth(0)
+    const calenderSel = new calenderSelector(page,rootCal,datePicker,monthChecker);
+    
     const cookieButton = page.locator('#acknowledgeDemoWarning');
     page.goto("https://phptravels.net/");
     await cookieButton.click();
-    await page.getByLabel('Departure Date').nth(0).click();
-    const str = await datePicker.nth(3).textContent();
-    console.log(str);
-    
+    await page.getByRole('tab',{ name: 'Flights' }).click();
+    await page.getByRole('button',{ name: 'Round Trip' }).click();
+    await calenderSel.dateSelector(deptOption,returnOption,4);
 })
--- playwright.local.config.ts
+++ playwright.local.config.ts
@@ -12,7 +12,8 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  //testDir: './e2e',
+  testDir: './fixtures',
+  testMatch: '**/*.spec.ts',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -37,7 +38,8 @@ export default defineConfig({
     {
       name: 'chromium',
       use: {
-        headless:false 
+        headless:false ,
+        screenshot: 'on'
         ,
         ...devices['Desktop Chrome'] },
     },
